6305489db05eb6b64aef392884ca10d21560910d
Crowphy <crowphy@qq.com>
2017-08-25 01:53:36 +0800

    --

--- this.js
+++ this.js
@@ -11,7 +11,7 @@ var obj = {
   getA: foo
 }
 bar(obj.getA); */
-var a = 20;
+/* var a = 20;
 var obj = {
     a: 10,
     c: this.a + 20,
@@ -21,4 +21,15 @@ var obj = {
 }
 
 console.log(obj.c);
-console.log(obj.fn());
+console.log(obj.fn()); */
+
+var obj = {
+  a: 10,
+  fn: function() {
+    console.log(this.a);
+    (function() {
+      console.log(this.a);
+    })()
+  }
+}
+obj.fn()
