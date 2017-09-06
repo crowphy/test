f22e9a078a9cdd261b797928b843a633b43b9db2
吴亚辉(平安官网) <wuyahui065@pingan.com.cn>
2017-09-06 15:41:51 +0800

    qs

--- quick-sort.js
+++ quick-sort.js
@@ -5,5 +5,22 @@
  * @param {any} arr 
  */
 function quickSort (arr) {
-    
+  if (arr.length <= 1) {
+    return arr
+  }
+  let index = Math.round(arr.length/2)
+  let q = arr.splice(index, 1)[0]
+  let left = [],
+      right = []
+  for (let item of arr) {
+    if (item < q) {
+      left.push(item)
+    } else {
+      right.push(item)
+    }
+  }
+  return quickSort(left).concat([q], quickSort(right))
 }
+const arr = [1, 5, 2, 4, 7, 3]
+const res = quickSort(arr)
+console.log(res)
