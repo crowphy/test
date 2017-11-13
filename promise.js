1382610fd2b69d9e704eceaa0d8bb58e1fa8b6ac
Crowphy <crowphy@qq.com>
2017-11-14 01:54:30 +0800

    gp

--- promise.js
+++ promise.js
@@ -17,30 +17,49 @@
 // })
 // // console.log('g1');
 
-function MyPromise(executor) {
-  this.queue = ['a']
-  executor(this.resolve, this.reject)
-}
-MyPromise.prototype.resolve = function(params) {
-  console.log(this.queue)
-  setTimeout(() => {
-    console.log(this.queue)
-    var fn = this.queue.shift()
-    fn.call(this, params)
-  })
-  console.log(this.queue)
-}
-MyPromise.prototype.reject = function (params) {
+// function MyPromise(executor) {
+//   this.queue = ['a']
+//   executor(this.resolve, this.reject)
+// }
+// MyPromise.prototype.resolve = function(params) {
+//   console.log(this.queue)
+//   setTimeout(() => {
+//     console.log(this.queue)
+//     var fn = this.queue.shift()
+//     fn.call(this, params)
+//   })
+//   console.log(this.queue)
+// }
+// MyPromise.prototype.reject = function (params) {
 
+// }
+// MyPromise.prototype.then = function (func) {
+//   this.queue.push(func)
+//   console.log(this.queue)
+//   return this
+// }
+// var p = new MyPromise(function(resolve, reject) {
+//   resolve('hello')
+// })
+// p.then(function(data) {
+//   console.log(data)
+// })
+function PromiseB(executor) {
+  var callbacks = []
+  this.then = function(callback) {
+    callbacks.push(callback)
+    return this
+  }
+  executor(resolve)
+  function resolve(value) {
+    setTimeout(() => {
+      var fn = callbacks.shift()
+      fn(value)
+    })
+  }
 }
-MyPromise.prototype.then = function (func) {
-  this.queue.push(func)
-  console.log(this.queue)
-  return this
-}
-var p = new MyPromise(function(resolve, reject) {
+var b = new PromiseB(function(resolve, reject) {
   resolve('hello')
-})
-p.then(function(data) {
+}).then(function (data) {
   console.log(data)
 })
