c6185fa1f40a38d446c8930610b566fb78b15963
crowphywu <crowphywu@tencent.com>
2021-05-11 17:15:45 +0800

    fix: 测试验证

--- crypto.js
+++ crypto.js
@@ -5,4 +5,4 @@ const md5 = crypto.createHash('md5');
 
 const uid = uuid.v4()
 console.log('uuid:', uid);
-console.log('md5:', md5.update('b061c140-cf8b-425e-98a9-ad1f50107a6e').digest('hex'));
+console.log('md5:', md5.update(uid).digest('hex'));
